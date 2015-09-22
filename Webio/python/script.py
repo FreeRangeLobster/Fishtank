a2991b2d85781d29013889d2da01dba8d6db78ca
freerangelobster <juanpadillavivas@gmail.com>
2015-09-22 19:41:32 +0000

    html  displaying temperature, still to tidy up code

--- Webio/python/script.py
+++ Webio/python/script.py
@@ -20,32 +20,31 @@ def setup():
     GPIO.setFunction(AIR, GPIO.OUT)
     GPIO.setFunction(AUX, GPIO.OUT)
     GPIO.setFunction(2, GPIO.OUT)
-
     import os
     os.system("sudo modprobe w1-gpio")
     os.system("sudo modprobe w1-therm")
 
-
     # retrieve current datetime
     now = datetime.datetime.now()
-
     # test if we are between ON time and tun the light ON
     if ((now.hour >= HOUR_ON) and (now.hour < HOUR_OFF)):
         GPIO.digitalWrite(LIGHT, GPIO.HIGH)
-
-
 # loop function is repeatedly called by WebIOPi 
+
+def measure():
+    global TempRead
+    tmp0 = webiopi.deviceInstance("tmp0")
+    # retrieves current temperature 
+    TempRead = tmp0.getCelsius() 
+    print("Temperature: %f" % TempRead)
+    return (TempRead)
+
+
+
+
 def loop():
     # retrieve current datetime
-    global temp0
     now = datetime.datetime.now()
-
-    #Temperature indicators
-    tmp0 = webiopi.deviceInstance("temp0")
-    TempRead = tmp0.getCelsius() # retrieve current temperature
-    print("Temperature: %f" % TempRead)
-
-
 
     # toggle light ON all days at the correct time
     if ((now.hour == HOUR_ON) and (now.minute == 0) and (now.second == 0)):
@@ -61,6 +60,17 @@ def loop():
     webiopi.sleep(1)
 
 # destroy function is called at WebIOPi shutdown
+
+
+@webiopi.macro
+def getSensor(arg0):
+    global TempRead
+    measure()
+    #print ("Sensor macro", int(value))
+    return TempRead
+
+
+
 def destroy():
     GPIO.digitalWrite(LIGHT, GPIO.LOW)
     GPIO.digitalWrite(PUMP, GPIO.LOW)
