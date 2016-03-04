9cac34a657b5618711c1052fcbef54bc8b3ed788
juanpadillavivas_mac <juanpadillavivas>
2016-03-04 20:31:04 +0000

    Arduino program prototype
    Arduino program prototype

---
+++ Arduino/Prototype/FishTank_ArduinoV1A.ino
@@ -0,0 +1,52 @@
+/* 
+Title:FishTank_Arduino_board
+
+Version             Description                          Date                       Changes
+1A              Testing communication with Pi         04/03/2016               Initial program
+
+Date: 04/03/2016
+
+Developer: Juan Vivas, www.freerangelobster.blogspot.co.uk, 
+
+Description:
+Recieves information from Raspberry Pi using the serial port. Updates 5 PWM channelsupdates the digital
+outputs and analog inputs
+For debugging purposes the SW serial por has been included, to enable it, set debug variable to 1
+Based on: Arduino code samples
+
+
+THE CIRCUIT
+In
+  Serial TR,RX:
+  Serial SW:
+  Analog input 0:
+
+Out
+  Channel1
+  Channel2
+  Channel3
+  Channel4
+  Channel5
+Note: The Arduino board is 5V based, the Raspberry pi board is 3.3V based. In order to couple the voltage between 
+the two boards it was required to use a voltage devider between the TX of the arduino and the RX of the Pi. The
+resistors used where 4k7 and 2k5
+
+COMMANDS
+Queries from Raspberry        Replies from Arduino      Command
+Arduino Status
+Read pressure
+
+Software serial enabled for debugging proposes
+*/
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
--- Arduino/Serial/Serial/Serial.ino
+++ Arduino/Serial/Serial/Serial.ino
@@ -1,13 +1,18 @@
-byte number = 0;
+int number = 0;
+int val=48;
 
 void setup(){
 Serial.begin(9600);
 }
 
 void loop(){
+  //val=val+1;
+  Serial.write("Hello"); 
   if (Serial.available()) {
     number = Serial.read();
-    Serial.print("character recieved: ");
-    Serial.println(number, DEC);
+    Serial.print("recieved");
+    //Serial.println(number, DEC);
+    //delay(500);
   }
+     delay(500);
 }
