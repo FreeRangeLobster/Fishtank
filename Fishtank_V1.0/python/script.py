4d905d81aa303bbd5eeb5086423a5fe91f3ef2d5
juanpadillavivas@gmail.com <juanpadillavivas@gmail.com>
2016-09-28 20:37:07 +0000

    Database prototype integrated and tested succesfully

--- Fishtank_V1.0/python/script.py
+++ Fishtank_V1.0/python/script.py
@@ -1,5 +1,10 @@
 import webiopi
 import datetime
+import MySQLdb
+ 
+
+db = MySQLdb.connect("localhost", "monitor", "password", "temps")
+curs=db.cursor() 
 
 GPIO = webiopi.GPIO
 from webiopi.devices.serial import Serial
@@ -104,6 +109,24 @@ def getSensor2(channel):
     measurePressure()
     return Pressure
 
+
+try:
+    curs.execute ("""INSERT INTO tempdat 
+            values(CURRENT_DATE(), NOW() - INTERVAL 12 HOUR, 'kitchen', 20.6)""")
+    db.commit()
+
+    print "After Commit"
+    curs.execute ("SELECT * FROM tempdat")
+    print "\nDate       Time        Zone        Temperature"
+    print "==========================================================="
+
+    for reading in curs.fetchall():
+        print str(reading[0])+" "+str(reading[1])+"     "+reading[2]+"      "+str(reading[3])
+
+
+except:
+   print "Error: the database is being rolled back"
+   db.rollback()
 
 # loop function is repeatedly called by WebIOPi 
 def loop():
